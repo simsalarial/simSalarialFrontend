c38243bcf44a1b025e6a97aeca11ebc4616cc349
Andre Bernardo <asoares.csc@gmail.com>
2019-08-14 20:01:08 +0100

    work-in-progress table
    Adapting dynamic fields to the table.

--- src/app/layout/admin/dashboard/dashboard.component.ts
+++ src/app/layout/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  columns: any = [];
+  rows: any = [];
+  temp: any = [];
+  @ViewChild(DatatableComponent, { static: false }) table: DatatableComponent;
+  
+  constructor() {
+    this.columns = [
+      { name: 'Comercial' },
+      { name: 'Consultor' },
+      { prop: 'simulacao_n' }
+    ];
+    this.temp = [{
+      comercial: 'Rita',
+      consultor: 'Jose',
+      simulacao_n: 1
+    },
+    {
+      comercial: 'Joana',
+      consultor: 'Ze Manel',
+      simulacao_n: 1
+    },
+    {
+      comercial: 'Joana',
+      consultor: 'Ze Manel',
+      simulacao_n: 2
+    }];
+    this.rows = this.temp;
+  }
 
   ngOnInit() {
+  }
+
+  updateFilter(event) {
+    const filterAttr =  'comercial';
+    const val = event.target.value.toLowerCase();
+    const temp = this.temp.filter((d) => {
+      return (d[filterAttr] != null) ? d[filterAttr].toLowerCase().indexOf(val) !== -1 : false;
+    });
+    this.rows = temp;
+    this.table.offset = 0;
   }
 
 }
