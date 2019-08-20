ea2025b42f677431b3c19f5407b43b6801129e18
Ricardo <ricardomartins216@gmail.com>
2019-08-20 03:57:42 +0100

    Various End To end to simulator

--- src/app/core/models/workInsurance.ts
+++ src/app/core/models/workInsurance.ts
@@ -2,6 +2,7 @@ export class WorkInsurance {
 
     'id'?: number;
     'workInsurancevariable': number;
+    'varAccountedForWorkInsurance': number;
 
     constructor(data?: any) {
         Object.assign(this, data);
--- src/app/core/services/data-service/data.service.ts
+++ src/app/core/services/data-service/data.service.ts
@@ -1,3 +1,5 @@
+import { FoodSubsidy } from './../../models/foodSubsidy';
+import { WorkInsurance } from './../../models/workInsurance';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -29,10 +31,37 @@ export class DataService {
     this.http.post('http://localhost:8080/simuladorsalarial/api/extras', extra);
   }
 
-  retrieveWorkInsurance(data) {
-    return this.http.get('http://localhost:8080/simuladorsalarial/api/workinsurance', data)
+  // WORK INSURANCE //
+
+  // tslint:disable-next-line: no-shadowed-variable
+  retrieveWorkInsurance(workInsurance) {
+    return this.http.get('http://localhost:8080/simuladorsalarial/api/workinsurance', workInsurance);
   }
 
+  postNewWorkInsuranceVariables(workInsurance) {
+    this.http.post('http://localhost:8080/simuladorsalarial/api/simulationfields/', workInsurance);
+  }
+
+  // MARGIN //
+
+  retrieveMarginValues(margin) {
+    return this.http.get('http://localhost:8080/simuladorsalarial/api/margin/', margin);
+  }
+
+  postNewMarginValues(margin) {
+    this.http.post('http://localhost:8080/simuladorsalarial/api/margin/newvalue', margin);
+  }
+
+
+  // FOOD SUBSIDY //
+
+  retrieveFoodSubsidyValue(foodSubsidyMonth) {
+    return this.http.get('http://localhost:8080/simuladorsalarial/api/foodsubsidy/', foodSubsidyMonth);
+  }
+
+  postNewFoodSubsidyValue(foodSubsidyMonth) {
+    this.http.put('http://localhost:8080/simuladorsalarial/api/foodsubsidy/newvalue', foodSubsidyMonth);
+  }
 
 
 }
